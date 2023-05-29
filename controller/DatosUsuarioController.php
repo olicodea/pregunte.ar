1085540d1c91ebfa4096b34f1e87ba01397925f4
Juan Ignacio Oliva <olicodea@gmail.com>
2023-05-29 10:44:56 -0300

    Regex pais ciudad

--- controller/DatosUsuarioController.php
+++ controller/DatosUsuarioController.php
@@ -6,7 +6,7 @@ class DatosUsuarioController
     private $datosUsuarioModel;
     private $nombreCompletoREGEX = "/^[A-Za-z\s]+$/";
     private $sexoREGEX = "/^(Masculino|Femenino|No binario)$/";
-    private $paisCiudadREGEX = "/^[A-Za-z\s]+,\s[A-Za-z\s]+$/";
+    private $paisCiudadREGEX = "/^[a-zA-Z\s,áéíóúÁÉÍÓÚñÑ]+$/";
     public function __construct($datosUsuarioModel, $renderer)
     {
         $this->renderer = $renderer;
@@ -20,16 +20,12 @@ class DatosUsuarioController
         $sexo = $_SESSION["Sexo"] ?? "";
         $paisCiudad = $_SESSION["PaisCiudad"] ?? "";
 
-        if($nombreCompleto != "" && $fechaNacimiento != "" && $sexo != "" && $paisCiudad != "") {
-            $data["NombreCompleto"] = $nombreCompleto;
-            $data["FechaNacimiento"] = $fechaNacimiento;
-            $data["Sexo"] = $sexo;
-            $data["PaisCiudad"] = $paisCiudad;
-            $data["sexos"] = $this->datosUsuarioModel->getSexos($sexo);
-            $this->renderer->render("datosUsuario", $data);
-        }
-
-        $this->renderer->render("datosUsuario");
+        $data["NombreCompleto"] = $nombreCompleto;
+        $data["FechaNacimiento"] = $fechaNacimiento;
+        $data["Sexo"] = $sexo;
+        $data["PaisCiudad"] = $paisCiudad;
+        $data["sexos"] = $this->datosUsuarioModel->getSexos($sexo);
+        $this->renderer->render("datosUsuario", $data);
     }
 
     public function validar() {
@@ -49,6 +45,7 @@ class DatosUsuarioController
             }
 
             if(!preg_match($this->paisCiudadREGEX, $_POST["PaisCiudad"])) {
+                echo $_POST["PaisCiudad"];
                 $error .= "País/ciudad no es válido.";
             }
         }

48f09b03584d5b9b6ab2deb3827b87676b5f855e
void_oli <olicodea@gmail.com>
2023-06-02 01:05:20 -0300

    Fix redireccion index

--- index.php
+++ index.php
@@ -7,11 +7,14 @@ $router = $configuration->getRouter();
 $module = $_GET['module'] ?: 'home';
 $method = $_GET['action'] ?: 'list';
 
-
 if(!isset($_SESSION["usuario"])) {
-    $module = $module == 'login' ? 'login': 'home';
+    $module = $module == 'login' ? 'login'
+        : ($module == 'registro' ? 'registro'
+            : ($module == 'datosLogin' ? 'datosLogin'
+                : ($module == 'datosUsuario' ? 'datosUsuario'
+                    : 'home')));
 } else {
-    if($module == 'login' || $module == 'home'){
+    if($module == 'login' || $module == 'home' || $module == 'registro' || $module == 'datosLogin' || $module == 'datosUsuario') {
         $module = 'lobby';
     }
 }

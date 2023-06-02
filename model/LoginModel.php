1a96c5f90783038e32849b5ac406c59d77fd025a
void_oli <olicodea@gmail.com>
2023-06-02 16:08:55 -0300

    Update - todo en login

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -7,13 +7,12 @@ class LoginModel{
         $this->database = $database;
     }
 
-    //TODO: Consulta para traer el usuario por nombreDeUsuario
     public function autenticarUsuario($usuario, $password) {
         $sql = "SELECT * FROM usuario WHERE nombreDeUsuario = ?";
         $resultado = $this->database->queryWthParameters($sql, $usuario);
         $usuarioBD = mysqli_fetch_assoc($resultado);
         $passwordhasheadea = md5($password);
-
+        //TODO: validar el rol del usuario que sea diferente a NoValidado
 
         return  $passwordhasheadea  == $usuarioBD["contrasenia"] ? $usuarioBD : false;
 

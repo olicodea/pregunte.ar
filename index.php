476c12c4e21df2159e2716bc5c1d9df1d69c3cf9
VillafaneNicolas <ndvruquet@gmail.com>
2023-06-03 18:37:08 -0300

    version 2.1 del index - Emprolijamiento

--- index.php
+++ index.php
@@ -6,13 +6,10 @@ $router = $configuration->getRouter();
 
 $noLoginUserSites = ["login","registro","datosLogin","datosUsuario","mailValidation"];
 
-
 $module = $_GET['module'] ?: 'home';
 $method = $_GET['action'] ?: 'list';
 
 $value = in_array($module, $noLoginUserSites);
-
-var_dump($value);
 
 if(!isset($_SESSION["usuario"])) {
     if(!in_array($module, $noLoginUserSites)){

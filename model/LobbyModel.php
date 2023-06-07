6f7fc3b9ad67d5234cf505139d7b9b0bbc564aff
Juan Ignacio Oliva <olicodea@gmail.com>
2023-06-07 15:50:26 -0300

    Ver partidas lobby
    - Model OK
    - Devuelve partidas con todos los datos al controller y la vista

--- model/LobbyModel.php
+++ model/LobbyModel.php
@@ -19,13 +19,31 @@ class LobbyModel
         return mysqli_fetch_assoc($result);
     }
     public function getLobbyPartidasJugadas($user){
-        $query = "SELECT p.idPartida as 'Partidas Jugadas', p.puntaje
+        $query = "SELECT p.puntaje, p.cantidadDeRespuestasAcertadas
                     FROM partida p
                     LEFT JOIN usuario u
                     ON p.idUsuario = u.idUsuario
                     WHERE u.nombreDeUsuario = ?
                     ORDER BY p.idPartida DESC";
+        $resultado = $this->database->queryWthParameters($query, $user);
+        $partidasJugadas = $this->crearArrayPartidas($resultado);
 
-        return $this->database->queryWthParameters($query, $user);
+        return $partidasJugadas;
+    }
+
+    private function crearArrayPartidas($resultado)
+    {
+        $partidasJugadas = [];
+
+        foreach ($resultado as $clave => $valor) {
+            $partidasJugadas[] = [
+                "nroPartida" => $clave + 1,
+                "puntaje" => $valor["puntaje"],
+                "cantidadDeRespuestasAcertadas" => $valor["cantidadDeRespuestasAcertadas"]
+            ];
+        }
+
+        $partidasInvertidas = array_reverse($partidasJugadas);
+        return $partidasInvertidas;
     }
 }

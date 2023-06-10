c805dce5cf5f5d0d12aa8cadd9c0bf1823bbf04a
Sebastian <sebacavs1995@gmail.com>
2023-06-10 12:35:59 -0300

    Se refactorizo el nombre de una variable para que se entienda mas

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ session_start();
 include_once('Configuration.php');
 $configuration = new Configuration();
 $router = $configuration->getRouter();
-$userSessionHelper = $configuration->getModuleHelper();
+$moduleHelper = $configuration->getModuleHelper();
 
 $module = $_GET['module'] ?: 'home';
 $method = $_GET['action'] ?: 'list';
 
-$userSessionHelper->chequearModulo($module, isset($_SESSION["usuario"]));
+$moduleHelper->chequearModulo($module, isset($_SESSION["usuario"]));
 
 $router->route($module, $method);
